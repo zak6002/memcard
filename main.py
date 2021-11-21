639d5393527379b30bbc1b3aff70f6fc74e7b1f7
zak6002 <91427659+zak6002@users.noreply.github.com>
2021-11-21 19:21:47 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QListWidget, QPushButton, QTextEdit, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout
+
+app = QApplication([])
+notes_win = QWidget()
+notes_win.setWindowTitle('Умные Заметки')
+notes_win.resize(900, 600)
+list_notes = QListWidget()
+list_notes_label = QLabel('Список заметок')
+
+button_note_create = QPushButton('Создать заметку')
+button_note_del = QPushButton('Удалить заметку')
+button_note_save = QPushButton('Сохранить заметку')
+
+list_tag = QListWidget()
+list_tag_label = QLabel('Список тегов')
+
+button_tag_add = QPushButton('Добавить тэг')
+button_tag_del = QPushButton('Удалить тэг')
+button_tag_search = QPushButton('Поиск по тэгу')
+
+field_text = QTextEdit()
+field_tag = QLineEdit()
+field_tag.setPlaceholderText('Введите тэг...')
+
+layout_notes = QHBoxLayout()
+col_1 = QVBoxLayout()
+col_2 = QVBoxLayout()
+row_1 = QVBoxLayout()
+row_2 = QVBoxLayout()
+row_3 = QVBoxLayout()
+row_4 = QVBoxLayout()
+col_1.addWidget(field_text)
+col_2.addWidget(list_notes_label)
+col_2.addWidget(list_notes)
+row_1.addWidget(button_note_create)
+row_1.addWidget(button_note_del)
+row_2.addWidget(button_note_save)
+col_2.addWidget(list_tag_label)
+col_2.addWidget(list_tag)
+col_2.addWidget(field_tag)
+row_3.addWidget(button_tag_add)
+row_3.addWidget(button_tag_del)
+row_4.addWidget(button_tag_search)
+
+col_2.addLayout(row_1)
+col_2.addLayout(row_2)
+col_2.addLayout(row_3)
+col_2.addLayout(row_4)
+
+layout_notes.addLayout(col_1, stretch = 2)
+layout_notes.addLayout(col_2, stretch = 1)
+notes_win.setLayout(layout_notes)
+notes_win.show()
+app.exec_()
+
