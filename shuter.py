bf270fa69257ffc0bee2e9bb1d663757087c8d67
zak6002 <91427659+zak6002@users.noreply.github.com>
2022-02-19 13:40:22 +0200

    Add files via upload

--- shuter.py
+++ shuter.py
@@ -6,8 +6,12 @@ mixer.music.load('Survive - Omniverse.mp3')
 mixer.music.play()
 fire_sound = mixer.Sound('fire.ogg')
 
+font.init()
+font2 = font.SysFont('Arial', 36)
+
 img_back = "galaxy.jpg"
 img_hero = "rocket.png"
+img_bullet = 'bullet.png'
 
 win_width = 700
 win_height = 500
@@ -37,9 +41,18 @@ class Player(GameSprite):
             self.rect.x -= self.speed
         if keys[K_d] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
-    def fire():
-        pass
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
+
 lost = 0
+score = 0
 
 class Enemy(GameSprite):
     def update(self):
@@ -50,19 +63,38 @@ class Enemy(GameSprite):
             self.rect.y = 0
             lost += 1
 
-enemy = Enemy('ufo.png', randint(10, 600), 0, 100, 100, randint(2, 5))
 rocet = Player(img_hero, 0, 350, 100, 130, 5)
+
+bullets = sprite.Group()
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy('ufo.png', randint(80, win_width - 80), 0, 100, 100, randint(1, 5))
+    monsters.add(monster)
 
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                rocet.fire()
     
     if not finish:
         window.blit(background, (0,0))
-        rocet.reset()
+        
+        text = font2.render('Счёт:' + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
+
+        text_lose = font2.render('Пропущено:' + str(lost), 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+
         rocet.update()
-        enemy.reset()
-        enemy.update()
+        monsters.update()
+        bullets.update()
+
+        rocet.reset()
+        monsters.draw(window)
+        bullets.draw(window)
         display.update()
     clock.tick(60)
